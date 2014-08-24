d1a87e5157099b4aeea594fc7d35096c04b8b67f
Lukas Heinrich <lukas.heinrich@gmail.com>
2014-08-23 22:13:54 -0400

    threads and confirm

--- server.py
+++ server.py
@@ -3,9 +3,9 @@
 import IPython
 
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for,redirect
 app = Flask(__name__)
-
+app.debug = True
 from flask.ext.socketio import SocketIO,emit
 socketio = SocketIO(app)
 
@@ -18,27 +18,47 @@ def upload_cards():
   #rudimentary.. better: http://flask.pocoo.org/docs/0.10/patterns/fileuploads/#uploading-files
   request.files['proccard'].save('uploads/proc_card.dat')
   request.files['paramcard'].save('uploads/param_card.dat')
-  return render_template('running.html')
+  return redirect(url_for('runview'))
 
 @app.route("/run")
 def runview():
   return render_template('running.html')
+
+p = None
+
+@socketio.on('killer')
+def killer():
+  print 'killing'
+  global p
+  if p is not None: 
+    p.kill()
+    print 'killed'
   
+def printer(req,p):
+  with app.test_request_context():
+    while True:
+      from flask import request
+      request = req
+      s =  p.stdout.readline()
+      if not s: break
+      import time
+      time.sleep(0.01)
+      import re
+      strip_ansi =  re.sub(r'\x1b\[([0-9,A-Z]{1,2}(;[0-9]{1,2})?(;[0-9]{3})?)?[m|K]?', '', s)
+      socketio.emit('respond',strip_ansi) 
+    return
+
 @socketio.on('runanalysis')
 def handle_my_custom_event():
   import subprocess
   print 'ok.. now run the analysis....'
+  global p 
   p = subprocess.Popen(['/Users/lukas/heptools/madgraph-1.5.10/bin/mg5','-f','mg5.cmd'],
                       stdout = subprocess.PIPE,
                       stderr = subprocess.PIPE)
-  while True:
-    s =  p.stdout.readline()
-    if not s: break
-    import time
-    time.sleep(0.01)
-    import re
-    strip_ansi =  re.sub(r'\x1b\[([0-9,A-Z]{1,2}(;[0-9]{1,2})?(;[0-9]{3})?)?[m|K]?', '', s)
-    emit('respond',strip_ansi)
+  import threading
+  t = threading.Thread(target=printer, args=(request,p))
+  t.start()
   print "emit some stuff here"
   return
   
